78f572c75b76d2197e82c27fb404c87ea8e2a1ca
YordanSV <yordan10_pv@hotmail.com>
2022-03-09 19:37:20 -0600

    tests capture the UserName and Password

--- app.js
+++ app.js
@@ -32,3 +32,91 @@ connection.on('connect', function(err) {
 });
 
 connection.connect();
+
+
+
+-------------------
+
+
+
+
+
+
+
+
+
+const { Connection, Request, TYPES } = require('node_modules/tedious');
+
+const config = {
+    server: '192.168.1.212',
+    authentication: {
+        type: 'default',
+        options: {
+            userName: 'test',
+            password: 'test'
+        }
+    },
+    options: {
+        port: 1433 // Default Port
+    }
+};
+
+const connection = new Connection(config);
+
+connection.connect(function(err) {
+    if (err) {
+        console.log('Connection Failed!');
+        throw err;
+    }
+
+});
+
+
+// using input parameters
+//--------------------------------------------------------------------------------
+function login() {
+    // Values contain variables idicated by '@' sign
+    const sql = `INSERT INTO ${table} (uniqueIdCol, intCol, nVarCharCol) VALUES (@uniqueIdVal, @intVal, @nVarCharVal)`;
+    const request = new Request(sql, (err, rowCount) => {
+        if (err) {
+            throw err;
+        }
+
+        console.log('rowCount: ', rowCount);
+        console.log('input parameters success!');
+        outputParameters();
+    });
+
+    // Setting values to the variables. Note: first argument matches name of variable above.
+    request.addParameter('uniqueIdVal', TYPES.UniqueIdentifier, 'ba46b824-487b-4e7d-8fb9-703acdf954e5');
+    request.addParameter('intVal', TYPES.Int, 435);
+    request.addParameter('nVarCharVal', TYPES.NVarChar, 'hello world');
+
+    connection.execSql(request);
+}
+
+// using output parameters
+//--------------------------------------------------------------------------------
+function outputParameters() {
+    const sql = 'SELECT @uniqueIdVal=uniqueIdCol, @intVal=intCol, @nVarCharVal=nVarCharCol from test_param';
+    const request = new Request(sql, (err, rowCount) => {
+        if (err) {
+            throw err;
+        }
+
+        console.log('output parameters success!');
+        console.log('DONE!');
+        connection.close();
+    });
+
+    // Emits 'returnValue' when executed.
+    request.addOutputParameter('uniqueIdVal', TYPES.UniqueIdentifier);
+    request.addOutputParameter('intVal', TYPES.Int);
+    request.addOutputParameter('nVarCharVal', TYPES.NVarChar);
+
+    request.on('returnValue', (paramName, value, metadata) => {
+        console.log(paramName + '=', value);
+    });
+
+    connection.execSql(request);
+}
